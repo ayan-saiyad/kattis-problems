29ed96934518d60ad52edb7202416d6d1cb0614a
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-02-04 16:23:43 -0600

    solved Tri

---
+++ src/Tri.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Tri {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        boolean found = true;
+        
+        if (c == a + b && found) {
+            pw.println(a + "+" + b + "=" + c);
+            found = false;
+        } else if (c == a - b && found) {
+            pw.println(a + "-" + b + "=" + c);
+            found = false;
+        } else if (c == a * b && found) {
+            pw.println(a + "*" + b + "=" + c);
+            found = false;
+        } else if (c == a / b && found) {
+            pw.println(a + "/" + b + "=" + c);
+            found = false;
+        } else if (a == b + c && found) {
+            pw.println(a + "=" + b + "+" + c);
+            found = false;
+        } else if (a == b - c && found) {
+            pw.println(a + "=" + b + "-" + c);
+            found = false;
+        } else if (a == b * c && found) {
+            pw.println(a + "=" + b + "*" + c);
+            found = false;
+        } else if (a == b / c && found) {
+            pw.println(a + "=" + b + "/" + c);
+            found = false;
+        }
+
+        br.close();
+        pw.close();
+    }
+}
